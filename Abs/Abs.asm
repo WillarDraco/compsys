2c4a83b3061ae25a5c0078b2c3c50f71bde3e829
willardraco <a1863235@adelaide.edu.au>
2023-09-15 12:46:37 +0930

    ABS

--- Abs/Abs.asm
+++ Abs/Abs.asm
@@ -1,4 +1,27 @@
 // Calculates the absolute value of R1 and stores the result in R0.
 // (R0, R1 refer to RAM[0], and RAM[1], respectively.)
 
-// Put your code here.
+@R1
+D = M;
+
+@NEGATIVE
+D;JLT
+@R0
+M = D;
+@END
+
+0;JMP
+
+(NEGATIVE)
+    @R1
+    D = !M
+    D = D + 1
+    @R0
+    M = D
+
+    @END
+    0;JMP
+(END)
+
+@END
+0;JMP
