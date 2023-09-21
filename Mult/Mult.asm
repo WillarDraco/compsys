a94fb28ee74027e5981586d3a5087be0fde14a54
willardraco <a1863235@adelaide.edu.au>
2023-09-21 23:13:17 +0930

    Mult

--- Mult/Mult.asm
+++ Mult/Mult.asm
@@ -6,4 +6,37 @@
 // Multiplies R1 and R2 and stores the result in R0.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@R0
+M = 0
+
+@R1
+D = M
+@END
+D;JEQ
+
+@R2
+D = M
+@END
+D;JEQ
+
+@R1
+D = M
+@R3
+M = D
+
+(MULT)
+@R2
+D = M
+
+@R0
+M = D + M
+
+@R3
+M = M - 1
+D = M
+@MULT
+D;JGT
+
+(END)
+@END
+0;JMP
