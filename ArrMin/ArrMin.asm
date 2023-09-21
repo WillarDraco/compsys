4b844218a1c1f24885e5af353891d22d3a0db13d
willardraco <a1863235@adelaide.edu.au>
2023-09-22 00:44:57 +0930

    ArrMin

--- ArrMin/ArrMin.asm
+++ ArrMin/ArrMin.asm
@@ -1,4 +1,72 @@
 // Finds the smallest element in the array of length R2 whose first element is at RAM[R1] and stores the result in R0.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@R0
+M = 0
+@ptr
+M = 0
+
+// Check array length if 0 end
+@R2
+D = M
+@END
+D;JLE
+
+//Puts first element as first min
+@R1
+A = M
+D = M
+@R0
+M = D
+
+//Copies pointer starting at the second element of the array
+@R1
+D = M
+@ptr
+M = D + 1
+
+@i
+M = 1
+
+//Comparing each value loop
+(MIN)
+@i
+D = M
+@R2
+D = D - M
+@END
+D;JEQ
+
+@ptr
+A = M
+D = M
+@R0
+D = D - M
+
+@LOOP
+D;JGE
+
+//New Min
+(NEWMIN)
+@ptr
+A = M
+D = M
+@R0
+M = D
+@LOOP
+0;JMP
+
+//Iterate the loop
+(LOOP)
+@i
+M = M + 1
+@ptr
+M = M + 1
+@MIN
+0;JMP
+
+
+
+(END)
+@END
+0;JMP
