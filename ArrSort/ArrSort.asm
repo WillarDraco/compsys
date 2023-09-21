2f4b4943e2f97fde61b188d86b4f3e37b3c5f002
willardraco <a1863235@adelaide.edu.au>
2023-09-22 02:29:44 +0930

    sort

--- ArrSort/ArrSort.asm
+++ ArrSort/ArrSort.asm
@@ -1,4 +1,115 @@
 // Sorts the array of length R2 whose first element is at RAM[R1] in ascending order in place. Sets R0 to True (-1) when complete.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@R1
+D = M - 1
+@R2
+M = M + D
+
+// Check array length if 0 end
+@R2
+D = M
+@END
+D;JLE
+
+
+(OUTER)
+	(CHECKOUT)
+	@R1
+	D = M
+	@R2
+	D = D - M
+	@END
+	D;JGT
+
+	@R1
+	D = M
+	@R3
+	M = D + 1
+
+(INNER)
+(CHECKINNER)
+	@R3
+	D = M
+	@R2
+	D = D - M
+	@INNERFIN
+	D;JGT
+	
+	@R3 //inner index
+	A = M
+	D = M //dereferenced value
+	@ELEM_POS
+	D;JGE
+	@ELEM_NEG
+	0;JMP
+
+//Swaps elements
+(SWAP)
+	@R1
+	A = M
+	D = M
+	@temp
+	M = D
+	@R3
+	A = M
+	D = M
+	@R1
+	A = M
+	M = D
+
+	@temp
+	D = M
+	@R3
+	A = M
+	M = D
+
+//Iterate the loop
+(INLOOP)
+	@R3
+	M = M + 1
+	@INNER
+	0;JMP
+
+(INNERFIN)
+	@R1
+	M = M + 1
+	@OUTER
+	0;JMP
+
+(REF_NEG)
+(REF_POS)
+	@R3
+	A = M
+	D = M
+	@R1
+	A = M
+	D = D - M
+	@LOOP
+	D;JGE
+	@SWAP
+	0;JMP
+
+(ELEM_NEG)
+	@R1
+	A = M
+	D = M
+	@REF_NEG
+	D;JLT
+	@LOOP
+	0;JMP
+
+(ELEM_POS)
+	@R1
+	A = M
+	D = M
+	@REF_POS
+	D;JGE
+	@LOOP
+	0;JMP
+
+(END)
+	@R0
+	M = -1
+	@END
+	0;JMP
